b08f95efd308c3c54b24a48012e5c48640a23cf7
Ubaldo-San-Juan <usan9fam@gmail.com>
2025-06-02 11:31:04 -0600

    IUserRepositoryCreated

--- src/USLabs.TaskManager.Data/Repositories/CategoryRepository.cs
+++ src/USLabs.TaskManager.Data/Repositories/CategoryRepository.cs
@@ -1,14 +1,17 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using USLabs.TaskManager.Data.Context;
 using USLabs.TaskManager.Data.Entities;
+using USLabs.TaskManager.Data.Repositories.Interfaces;
 
 namespace USLabs.TaskManager.Data.Repositories
 {
     public class CategoryRepository
     {
-
-
+        
     }
 }
