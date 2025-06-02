c8533a12cd636eb6c20c6b17b25727351c79aefb
Ubaldo-San-Juan <usan9fam@gmail.com>
2025-06-01 18:07:58 -0600

    User Entity Created

---
+++ src/USLabs.TaskManager.Data/Entities/User.cs
@@ -0,0 +1,30 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace USLabs.TaskManager.Data.Entities
+{
+    public class User
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string? FirstName { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string? LastName { get; set; }
+        [Required]
+        [MaxLength(250)]
+        [EmailAddress]
+        public string? Email { get; set; }
+        [Required]
+        [MaxLength(255)]
+        public string? PasswordHash { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; }
+        public bool IsActive { get; set; } = true;
+
+        // Navigation properties
+
+    }
+}
