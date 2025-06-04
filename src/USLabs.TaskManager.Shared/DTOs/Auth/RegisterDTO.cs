292288ea2aaff36cf9f76652c657ea1384d9638e
Ubaldo-San-Juan <usan9fam@gmail.com>
2025-06-03 22:48:39 -0600

    AuthResponseDTO Created

---
+++ src/USLabs.TaskManager.Shared/DTOs/Auth/RegisterDTO.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace USLabs.TaskManager.Shared.DTOs.Auth
+{
+    public class RegisterDTO
+    {
+        
+    }
+}
